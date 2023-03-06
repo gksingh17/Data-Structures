59ab49165bf89d6385ece179e235e486f6d15c24
gksingh17 <gunjankumarsingh17@gmail.com>
2023-03-06 18:02:59 +0000

    folder management

---
+++ Python/leetcode/arrays/runningsumarray.py
@@ -0,0 +1,17 @@
+# Input: nums = [1,2,3,4]
+# Output: [1,3,6,10]
+# Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(1, len(nums)):
+            nums[i]= nums[i] + nums[i-1]
+        return nums
+
+
+nums = [1,2,3,4]
+sol = Solution()
+print(sol.runningSum(nums))
